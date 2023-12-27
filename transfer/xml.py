f41ca06b0c5263177f4bf29d8ee4b077b96841c2
Joshua Beretta <joshuaberetta@gmail.com>
2023-12-27 17:30:56 +0200

    add mapping from filename to submission value

--- transfer/xml.py
+++ transfer/xml.py
@@ -8,6 +8,7 @@ from datetime import datetime
 from xml.etree import ElementTree as ET
 
 from .media import get_media, del_media
+from utils.text import get_valid_filename
 from helpers.config import Config
 
 
@@ -23,6 +24,19 @@ def get_submission_edit_data():
     return data
 
 
+def get_all_values_from_xml(elem):
+    values = []
+    for child in elem:
+        values.extend(get_all_values_from_xml(child))
+    if elem.text:
+        values.append(elem.text.strip())
+    return [v for v in values if v]
+
+
+def get_xml_value_media_mapping(values):
+    return {get_valid_filename(v):v for v in values}
+
+
 def get_src_submissions_xml(xml_url):
     config = Config().src
     res = requests.get(
@@ -33,7 +47,7 @@ def get_src_submissions_xml(xml_url):
     return ET.fromstring(res.text)
 
 
-def submit_data(xml_sub, _uuid, original_uuid):
+def submit_data(xml_sub, _uuid, original_uuid, xml_value_media_map):
     config = Config().dest
 
     file_tuple = (_uuid, io.BytesIO(xml_sub))
@@ -45,7 +59,8 @@ def submit_data(xml_sub, _uuid, original_uuid):
     )
     for file_path in glob.glob(submission_attachments_path):
         filename = os.path.basename(file_path)
-        files[filename] = (filename, open(file_path, 'rb'))
+        filename_value = xml_value_media_map.get(filename)
+        files[filename_value] = (filename_value, open(file_path, 'rb'))
 
     res = requests.Request(
         method='POST',
@@ -117,7 +132,15 @@ def transfer_submissions(all_submissions_xml, asset_data, quiet, regenerate):
             submission_xml, 'formhub/uuid', asset_data['formhub_uuid']
         )
 
-        result = submit_data(ET.tostring(submission_xml), _uuid, original_uuid)
+        submission_values = get_all_values_from_xml(submission_xml)
+        xml_value_media_map = get_xml_value_media_mapping(submission_values)
+
+        result = submit_data(
+            ET.tostring(submission_xml),
+            _uuid,
+            original_uuid,
+            xml_value_media_map,
+        )
         if result == 201:
             msg = f'✅ {_uuid}'
         elif result == 202:
